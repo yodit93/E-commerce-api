f9fd511a954379d9c88eb0555dcb150710c6909b
yodit93 <yoditabebe9391@gmail.com>
2023-09-07 17:29:30 +0300

    Use devise for a User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :orders, foreign_key: :customer_id, dependent: :destroy
   validates :username, presence: true, uniqueness: true
   validates :address, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
     resources :users, only: [:index, :show] do
       resources :orders, only: [:index, :show]
     end
